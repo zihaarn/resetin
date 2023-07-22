93021df7206f03a4323305664ec5b8b2eac30fb0
Zihaarrnn <90699089+zihaarrnn@users.noreply.github.com>
2023-07-22 23:49:46 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/home' element={<Home />} />
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import { useNavigate } from "react-router-dom";
+import '../css/Home.css'
+export default function Home() {
+    const navigate = useNavigate()
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
