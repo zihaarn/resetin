8b2157367860c2610e3f62f29ab652a99f9fa5da
Zihaarrnn <90699089+zihaarrnn@users.noreply.github.com>
2023-07-23 20:03:35 +0500

    Update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,6 +16,16 @@ export default function Home() {
                 </nav>
             </header>
 
+            <p className="starting-heading">
+            Streamline Your Coding Productivity Today!
+            </p>
+
+            <p className="starting-intro">
+                Elevate your life and productivity. 
+                Cultivate positive habits with our comprehensive platform,
+                empowering routines for fulfillment.
+            </p>
+
             <button className="get-started-btn" onClick={() => navigate('waitlist')}>
                 <span className="get-started-btn-text">Get Started</span>
                 <ArrowIcon className="get-started-btn-icon" />
@@ -25,6 +35,27 @@ export default function Home() {
                <div className="lightspot-line"></div>
                <div className="lightspot"></div>
             </div>
+
+
+            <button className="get-started-btn" onClick={() => navigate('waitlist')}>
+                <span className="get-started-btn-text">Get Started</span>
+                <ArrowIcon className="get-started-btn-icon" />
+            </button>
+
+            <div>
+               <div className="lightspot-line"></div>
+               <div className="lightspot"></div>
+            </div>
+
+            <section className="footer-cont">
+                <div className="legal-div">
+                    <p>Legal</p>
+                    <div className="legal-links">
+                    <a href="#">Privacy Policy</a>
+                    <a href="#">Terms</a>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
